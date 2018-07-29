d919799e556a54bb7dbc0eaa2ed377ba1f1ecb16
endymion Zhou <31771433+tweetyZCX@users.noreply.github.com>
2018-07-29 22:57:10 +0800

    学完了react之后，做了一个很粗糙的类简书网站。后端采用node.js。

---
+++ 低配版简书整站 with React/src/app.js
@@ -0,0 +1,24 @@
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+
+import Frame from 'frame/Frame';
+
+require('semantic/dist/semantic.min.css');
+require('semantic/dist/semantic.min.js');
+
+$.ajaxSetup({
+    xhrFields: {withCredentials: true}
+});
+
+ReactDOM.render(
+    <Router>
+        <Route path="/" component={Frame}/>
+    </Router>,
+    document.getElementById('root')
+);
+
+if (module.hot) {
+    module.hot.accept();
+}
